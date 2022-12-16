056856eedbfdef1a1cb27e162ff286703869892e
seokhh <119908334+seokhh@users.noreply.github.com>
2022-12-16 15:45:57 +0900

    Add files via upload

---
+++ 백준 3003번.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 1;
+        int b = 1;
+        int c = 2;
+        int d = 2;
+        int e = 2;
+        int f = 8;
+
+        a = a - sc.nextInt();
+        b = b - sc.nextInt();
+        c = c - sc.nextInt();
+        d = d - sc.nextInt();
+        e = e - sc.nextInt();
+        f = f - sc.nextInt();
+
+
+        System.out.print(a+ " ");
+        System.out.print(b+ " ");
+        System.out.print(c+ " ");
+        System.out.print(d+ " ");
+        System.out.print(e+ " ");
+        System.out.print(f+ " ");
+    }
+}
