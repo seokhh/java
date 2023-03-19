71e42c215d6c1080b43006651e25e41b81f49a9f
seokhh <119908334+seokhh@users.noreply.github.com>
2023-03-19 18:51:56 +0900

    Add files via upload

---
+++ Your_avg.java
@@ -0,0 +1,20 @@
+package Baekjoon.problem.no25206;
+import java.util.Scanner;
+
+public class Your_avg {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        /*String[] arr = new String[20];
+
+        int sum = 0;
+        double avg = 0;*/
+        System.out.println("20개의 과목명, 학점, 등급을 기재하시오");
+        for(int i = 0; i< 20; i++) {
+            String sub = sc.nextLine();
+            double credit = sc.nextInt();
+            String grade = sc.nextLine();
+            }
+
+    }
+}
